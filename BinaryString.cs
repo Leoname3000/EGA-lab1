572d0925e922701df3bc7d00caed12fdd1d67aca
Leonid Sulin <sulin.leonid@gmail.com>
2022-10-01 20:59:26 +0300

    Initial commit

---
+++ BinaryString.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace EGA_lab1
+{
+    public class BinaryString
+    {
+        public const int MU_FORMAT_WIDTH = 9;
+        public const int CTR_FORMAT_WIDTH = 2;
+
+        const int MOD_B = 2;
+        const int L = 15;
+
+        public string StrVal;
+
+        public BinaryString(int rawInt)
+        {
+            string binaryString = "";
+            int remainder;
+
+            while (rawInt > 0)
+            {
+                remainder = rawInt % MOD_B;
+                binaryString = $"{remainder}" + binaryString;
+                rawInt /= MOD_B;
+            }
+            for (int i = 0; binaryString.Length < L; i++)
+            {
+                binaryString = "0" + binaryString;
+            }
+            StrVal = binaryString;
+        }
+
+        public BinaryString()
+        {
+            StrVal = Generate();
+        }
+
+        private string Generate()
+        {
+            Random random = new Random();
+
+            string binaryString = "";
+            for (int i = 0; i < L; i++)
+            {
+                binaryString = $"{random.Next(0, 10) % MOD_B}" + binaryString;
+            }
+            return binaryString;
+        }
+
+        public int IntVal()
+        {
+            int result = 0;
+
+            for (int i = 0; i < L; i++)
+            {
+                result += Convert.ToInt32($"{StrVal[i]}") * Convert.ToInt32(Math.Pow(MOD_B, L - (i + 1)));
+            }
+            return result;
+        }
+        
+        public double Mu()
+        {
+            return Math.Pow(Convert.ToDouble(IntVal()) - Math.Pow(2, L - 1), 2);
+
+        }
+
+        public static void CreateLandscape(int length, int start = 0, int step = 1)
+        {
+            for (int i = start; i < start + length; i += step)
+            {
+                BinaryString landscapeString = new BinaryString(i);
+                Console.WriteLine($"{i,CTR_FORMAT_WIDTH} = {landscapeString.StrVal}, mu = {landscapeString.Mu(), MU_FORMAT_WIDTH}");
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using EGA_lab1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("------======= LANDSCAPE =======------\n");
+        BinaryString.CreateLandscape(32);
+
+        Console.WriteLine("\n------======= MONTE-CARLO METHOD =======------\n");
+        Console.WriteLine("Enter N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+        List<BinaryString> S = new List<BinaryString>();
+        BinaryString maxS = new BinaryString();
+        double max = -1;
+
+        for (int i = 0; i < N; i++)
+        {
+            bool maxChanged = false;
+            string reportString = $"Step {i + 1, BinaryString.CTR_FORMAT_WIDTH}: ";
+
+            S.Add(new BinaryString());
+            reportString += $"S[i] = {S[i].StrVal}, mu = {S[i].Mu(), BinaryString.MU_FORMAT_WIDTH}, ";
+
+            if (max < S[i].Mu())
+            {
+                max = S[i].Mu();
+                maxS = S[i];
+
+                if (i != 0)
+                    maxChanged = true;
+            }
+
+            reportString += $"maxS = {maxS.StrVal}, max = {max, BinaryString.MU_FORMAT_WIDTH}";
+
+            if (maxChanged && i != 0)
+                reportString += " <- CHANGE";
+
+            Console.WriteLine(reportString);
+        }
+        Console.WriteLine($"\n> Final result: {maxS.StrVal}, mu = {max, BinaryString.MU_FORMAT_WIDTH}.");
+    }
+}
